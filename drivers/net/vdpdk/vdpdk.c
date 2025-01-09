f4427b06ed0f4625d0c807385c5e8f2ccd372d9b
Dominik Kreutzer <kreutzer@in.tum.de>
2025-01-09 16:42:46 +0100

    Log queue zones

--- drivers/net/vdpdk/vdpdk.c
+++ drivers/net/vdpdk/vdpdk.c
@@ -315,6 +315,8 @@ vdpdk_rx_queue_setup(struct rte_eth_dev *dev,
 		          (unsigned)nb_desc, ring_elements, ring_size);
 		return -ENOMEM;
 	}
+	// TODO: turn into trace
+	VDPDK_LOG(INFO, "queue: %d, addr: %p, iova: %"PRIx64", size: %zx", (int)queue_idx, ring->addr, (uint64_t)ring->iova, ring_size);
 	memset(ring->addr, 0, ring_size);
 
 	// Allocate mbufs
